37bc700a87217b8e4d511a85ec0d232d7877868c
Felipe Menanteau <felipe@illinois.edu>
2015-07-31 14:12:20 +0000

    template changes to surrender node back

--- scripts/worker-CC-template-condor_8_2_6.sh
+++ scripts/worker-CC-template-condor_8_2_6.sh
@@ -20,7 +20,7 @@ export _condor_NUM_CPUS={NCPU}
 export _condor_UID_DOMAIN=campuscluster.illinois.edu
 export _condor_FILESYSTEM_DOMAIN=campuscluster.illinois.edu
 export _condor_MAIL=/bin/mail
-export _condor_STARTD_NOCLAIM_SHUTDOWN=1800
+export _condor_STARTD_NOCLAIM_SHUTDOWN={NO_CLAIM_SHUTDOWN}
 
 # Extra conf for IP address
 export _condor_COLLECTOR_HOST={IP_SUBMIT_SITE} 
@@ -33,7 +33,7 @@ echo $psef
 if [[ "$psef" == *condor_master* ]]
 then
   echo "condor_master is already running on this node.";
-  sleep 36000
+  sleep 3600
 else
   #
   mkdir -p ${HOME}/condor_local
@@ -44,14 +44,8 @@ else
   #
   echo "Script_Launching condor master";
   echo ${_condor_SBIN}/condor_master
-  ${_condor_SBIN}/condor_master
+  ${_condor_SBIN}/condor_master -f
 fi
 
-echo WAITING 
-sleep 120
-
-tail -f  ${HOME}/condor_local/desdm/${local_host_name}/log/MasterLog
-
-wait
-
+echo "Finishing worker"
 
