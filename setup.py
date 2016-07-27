505e106cc3a29ba93c0c45f6163930f7a031e11a
Felipe Menanteau <felipe@illinois.edu>
2016-07-27 15:30:03 -0500

    version changes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ bin_files = ["bin/create-glidein",]
 
 # The main call
 setup(name='desglidein',
-      version ='0.1.5',
+      version ='0.2.4',
       license = "GPL",
       description = "Python function to create gliden in from condos/shell/pbs libraries",
       author = "Felipe Menanteau",
